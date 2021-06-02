d622168f4b33397a9c9936b14acfd7899466905b
qamk <qamking@gmail.com>
2021-06-02 12:47:59 +0100

    Add more examples

--- sum-count-groupby.sql
+++ sum-count-groupby.sql
@@ -14,3 +14,23 @@ GROUP BY continent;
 SELECT continent FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000;
+
+/* NOBEL TABLE */
+-- Selecting the number of different winners per subject
+SELECT subject, COUNT(DISTINCT winner) '# winners' FROM nobel
+GROUP BY subject;
+
+-- Selecting the number of years with awards
+SELECT subject, COUNT(DISTINCT yr) FROM nobel
+GROUP BY subject;
+
+-- Selecting winners with more than one prize
+SELECT DISTINCT winner FROM nobel
+GROUP BY winner
+HAVING COUNT(winner) > 1;
+
+-- Selecting year and subject with more than 3 prizes given after 2000 (inclusive)
+SELECT yr, subject FROM nobel
+WHERE yr >= 2000
+GROUP BY yr, subject
+HAVING COUNT(winner) = 3;
